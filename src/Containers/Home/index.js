a0f974403ea87f8efc0a6e1a636c8a2ac2920b8f
luigiolivi <eterowiczluigi@gmail.com>
2024-01-31 12:59:34 -0300

    import styled simplified

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -12,41 +12,41 @@ import MagnifyingImg from '../../assets/magnifying.png'
 import Person from '../../assets/person.png'
 import MainImageImg from '../../assets/teste.png'
 
-import { AIcons, ASections, AboutDescription, AboutTitle, AboutWrapper, CarouselAllItems, CarouselTitle, CarouselWrapper, Container, DivHeaderIcons, DivInput, DownArrow, FooterBar, Header, HeaderBar, HeaderSections, Icons, Input, Logo, Magnifying, LabelLogin, Main, MainHeader, MainImage, SearchInputButton } from './styles'
+import * as S from './styles'
 
 function Home () {
   return (
-    <Container>
-      <Header>
-        <MainHeader>
-          <Logo src={LogoImg} alt='logo' />
-          <DivInput>
-            <Input placeholder='O que está buscando?'/>
-            <SearchInputButton><Magnifying src={MagnifyingImg} alt='magnifying-glass-icon' /></SearchInputButton>
-          </DivInput>
+    <S.Container>
+      <S.Header>
+        <S.MainHeader>
+          <S.Logo src={LogoImg} alt='logo' />
+          <S.DivInput>
+            <S.Input placeholder='O que está buscando?'/>
+            <S.SearchInputButton><S.Magnifying src={MagnifyingImg} alt='magnifying-glass-icon' /></S.SearchInputButton>
+          </S.DivInput>
 
-          <DivHeaderIcons>
-            <Icons src={Person} alt='icone-person' /><AIcons><LabelLogin>Entrar / Cadastrar</LabelLogin><br /> Minha conta <DownArrow src={DownArrowImg}/></AIcons>
-            <Icons src={Bag} alt='icone-sacola' /><AIcons>Meus pedidos</AIcons>
-            <Icons src={Cart} alt='icone-carrinho' /><AIcons>Carrinho</AIcons>
-          </DivHeaderIcons>
-        </MainHeader>
+          <S.DivHeaderIcons>
+            <S.Icons src={Person} alt='icone-person' /><S.AIcons><S.LabelLogin>Entrar / Cadastrar</S.LabelLogin><br /> Minha conta <S.DownArrow src={DownArrowImg}/></S.AIcons>
+            <S.Icons src={Bag} alt='icone-sacola' /><S.AIcons>Meus pedidos</S.AIcons>
+            <S.Icons src={Cart} alt='icone-carrinho' /><S.AIcons>Carrinho</S.AIcons>
+          </S.DivHeaderIcons>
+        </S.MainHeader>
 
-        <HeaderSections>
-          <ASections>Início</ASections>
-          <ASections>Catálogo</ASections>
-          <ASections>Categorias <DownArrow src={DownArrowImg} /></ASections>
-        </HeaderSections>
+        <S.HeaderSections>
+          <S.ASections>Início</S.ASections>
+          <S.ASections>Catálogo</S.ASections>
+          <S.ASections>Categorias <S.DownArrow src={DownArrowImg} /></S.ASections>
+        </S.HeaderSections>
 
-        <HeaderBar src={HeaderBarImg}/>
-      </Header>
+        <S.HeaderBar src={HeaderBarImg}/>
+      </S.Header>
 
-      <Main>
-        <MainImage src={MainImageImg}/>
+      <S.Main>
+        <S.MainImage src={MainImageImg}/>
 
-        <CarouselWrapper>
-          <CarouselTitle>Ofertas</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
+        <S.CarouselWrapper>
+          <S.CarouselTitle>Ofertas</S.CarouselTitle>
+          <S.CarouselAllItems>Ver todos</S.CarouselAllItems>
           <Carousel itemsToShow={4}>
             <div>1</div>
             <div>2</div>
@@ -54,11 +54,11 @@ function Home () {
             <div>4</div>
             <div>5</div>
           </Carousel>
-        </CarouselWrapper>
+        </S.CarouselWrapper>
 
-        <CarouselWrapper>
-          <CarouselTitle>Feminino</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
+        <S.CarouselWrapper>
+          <S.CarouselTitle>Feminino</S.CarouselTitle>
+          <S.CarouselAllItems>Ver todos</S.CarouselAllItems>
           <Carousel itemsToShow={4}>
             <div>1</div>
             <div>2</div>
@@ -66,64 +66,16 @@ function Home () {
             <div>4</div>
             <div>5</div>
           </Carousel>
-        </CarouselWrapper>
+        </S.CarouselWrapper>
 
-        <CarouselWrapper>
-          <CarouselTitle>Masculino</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
-          <Carousel itemsToShow={4}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-          </Carousel>
-        </CarouselWrapper>
+        <S.AboutWrapper>
+          <S.AboutTitle>Sobre nós</S.AboutTitle>
+          <S.AboutDescription>Somos uma empresa top de perfumes</S.AboutDescription>
+        </S.AboutWrapper>
 
-        <CarouselWrapper>
-          <CarouselTitle>Unissex</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
-          <Carousel itemsToShow={4}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-          </Carousel>
-        </CarouselWrapper>
-
-        <CarouselWrapper>
-          <CarouselTitle>Infantil</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
-          <Carousel itemsToShow={4}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-          </Carousel>
-        </CarouselWrapper>
-
-        <CarouselWrapper>
-          <CarouselTitle>Ambiente</CarouselTitle>
-          <CarouselAllItems>Ver todos</CarouselAllItems>
-          <Carousel itemsToShow={4}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-          </Carousel>
-        </CarouselWrapper>
-
-        <AboutWrapper>
-          <AboutTitle>Sobre nós</AboutTitle>
-          <AboutDescription>Somos uma empresa top de perfumes</AboutDescription>
-        </AboutWrapper>
-
-        <FooterBar src={FooterBarImg} alt='footer-bar-img'/>
-      </Main>
-    </Container>
+        <S.FooterBar src={FooterBarImg} alt='footer-bar-img'/>
+      </S.Main>
+    </S.Container>
   )
 }
 
