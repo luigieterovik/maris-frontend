a0669ca858f81df3c25d8d94689f0276e5b02104
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-04-20 19:50:11 -0300

    pagiation added at FilteredProducts

--- src/Containers/FilteredProducts/index.js
+++ src/Containers/FilteredProducts/index.js
@@ -1,12 +1,42 @@
 import React from 'react'
 
+import CarouselItem from '../../components/CarouselItem'
+import Pagination from '../../components/Pagination'
+
+import { productsState } from '../../utils/states'
+
 import * as S from './styles'
 export default function FilteredProducts() {
-    return (
-        <S.Wrapper>
-            <S.ProductsWrapper>
+  const { products } = productsState
 
-            </S.ProductsWrapper>
-        </S.Wrapper>
-    )
+  return (
+    <S.Wrapper>
+      <S.ProductsWrapper>
+        <S.Title>Produtos com &quot;&quot;</S.Title>
+        <S.WrapperProducts>
+          {products.map(product => (
+            <CarouselItem
+              key={product.id}
+              isOffer={product.isOffer}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              offerPercentage={product.isOffer && product.offerPercentage}
+              installment={product.installment}
+              isCatalogue
+            />
+          ))}
+
+          <Pagination
+            totalProducts={products.length}
+            productsPerPage={productsPerPage}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+            totalFilteredProducts={filteredProducts.length}
+          />
+        </S.WrapperProducts>
+      </S.ProductsWrapper>
+    </S.Wrapper>
+  )
 }
--- src/Containers/FilteredProducts/styles.js
+++ src/Containers/FilteredProducts/styles.js
@@ -3,6 +3,23 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     background-color: #f7f7f7;
     width: 100%;
+
+    display: flex;
+    justify-content: center;
 `
 
-export const ProductsWrapper = styled.div``
+export const ProductsWrapper = styled.div`
+    background-color: white;
+    width: 90%;
+    border-radius: 20px;
+`
+
+export const Title = styled.h2``
+
+export const WrapperProducts = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+  margin-left: 15px;
+`
